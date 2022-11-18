2fce2f6d9d0bae4792741f62c8340318851673c3
nicole595 <nicoletp16@gmail.com>
2022-11-18 08:00:02 -0300

    Vercel

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-
 import * as S from "./styles";
 import { ButtonComponent } from "components";
 import { useAuth } from "hooks/authHook";
@@ -58,6 +57,7 @@ const Login = () => {
         </p>
       </form>
     </S.Section>
+    
   );
 };
 
